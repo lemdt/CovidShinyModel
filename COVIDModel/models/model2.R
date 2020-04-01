64f1ce259ea31e50ca89b7f8d783e0fcc31e77c2
root <root@UC80-409-8588.campus.net.ucsf.edu>
2020-04-01 15:20:21 -0700

    adding some documentation on model 2 functions

--- COVIDModel/models/model2.R
+++ COVIDModel/models/model2.R
@@ -2,13 +2,11 @@
 ##  Model 2 Main Code
 ##  ............................................................................
 
-library(data.table)
-
-#' Transition matrix
+#' Generate transition matrix
 #'
-#' Creates Markov transition matrix using the transition probabilties. 
-#' 
-#' The transition model is used 
+#' This function creates the Markov transition matrix using the transition probabilties. 
+#' This transition matrix will be used in the Markov model, as diagramed here: 
+#' https://drive.google.com/file/d/1_A3Q6C46z4hx02ayqVPhbWuIWFy6Y8ln/view?usp=sharing
 #'
 #' @param p.g_g Numeric. Transition probability from G -> G state. 
 #' @param p.g_icu Numeric. Transition probability from G -> ICU state. 
@@ -22,7 +20,7 @@ library(data.table)
 #' @param p.g_m Numeric. Transition probability from G -> M state. 
 #' @param p.icu_m Numeric. Transition probability from ICU -> M state. 
 #'
-#' @return The transition matrix used in the update step of the Markov model. 
+#' @return Transition matrix, to be used in the update step of the Markov model. 
 createTransition <- function(p.g_g, 
                              p.g_icu, 
                              p.g_d, 
@@ -44,7 +42,21 @@ createTransition <- function(p.g_g,
   return(tmat)
 }
 
-# Run Markov Simulation 
+
+#' Run Markov Simulation
+#' 
+#' This function takes in as inputs a vector of new hospitalizations per day and 
+#' runs the Markov simulation using this.
+#' 
+#' It returns a dataframe with the number of people in the general hospital (G.state),
+#' non-ventilated ICU (ICU.state), ventilated ICU (V.state), discharged (DCH.state), 
+#' and dead (M.state).
+#'
+#' @param new.g.vec Vector of numerics, which describe the number of new hospitalizations 
+#' per day. 
+#' @param trans.mat Matrix. For Markov simulation.  
+#'
+#' @return Dataframe, with counts of each state. 
 runMarkov <- function(new.g.vec, trans.mat){
   
   # h, icu, vent, d, m 
@@ -62,9 +74,34 @@ runMarkov <- function(new.g.vec, trans.mat){
   return(df.final)
 }
 
-# Runs SEIR, combines with Markov
+
+#' Run SEIR model 
+#' 
+#' Runs the SEIR model, incorporating the separate hospitalization Markov component. 
+#' More about the technique is here: 
+#' https://drive.google.com/file/d/157yB7O62xyJps2_HUtAelqsQ8e9feTj_/view?usp=sharing
+#' https://sites.me.ucsb.edu/~moehlis/APC514/tutorials/tutorial_seasonal/node4.html
+#' 
+#' The simulation ignores births and deaths.  
+#'
+#' @param S0 Numeric. Initial number of susceptible individuals. 
+#' @param E0 Numeric. Initial number of exposed individuals. 
+#' @param I0 Numeric. Initial number of infected individuals. 
+#' @param R0 Numeric. Initial number of recovered individuals.
+#' @param beta.vector Vector, of beta values to use in the simulation on each day. 
+#' Should have the same length as num.days.  
+#' @param num.days Numeric. Number of days to run the simulation. 
+#' @param influx List. This is hash consisting of a 'day' and an 'influx' number, 
+#' meant to represent an influx of infections into the region 
+#' @param params List. This should contain the parameters for running the SEIR, 
+#' including sigma, gamma.r, gamma.h, hosp.rate, and the transition probabilities 
+#' (p.g_g, p.g_icu, p.g_d)
+#'
+#' @return Dataframe, with susceptible counts, exposed counts, infected counts, 
+#' hospitalization numbers, recovery numbers, and death numbers. 
 SEIR <- function(S0, E0, I0, R0, beta.vector, 
                  num.days, influx, params) {
+ 
   # get parameters
   sigma <- params$sigma
   gamma.r <- params$gamma.r
@@ -105,6 +142,7 @@ SEIR <- function(S0, E0, I0, R0, beta.vector,
   R[1] <- R0
   newG[1] <- 0
   
+  # sum of the entire population 
   N = S[1] + E[1] + IR[1] + IH[1] + R[1]
 
   # run SIR model 
@@ -129,27 +167,44 @@ SEIR <- function(S0, E0, I0, R0, beta.vector,
     }
   }
   
-  I <- IR + IH
+  # create initial dataframe
+  df.return <- data.frame(day = 0:(num.days-1), S, E, IR, IH, R, newG)
   
-  df.return <- data.frame(day = 0:(num.days-1), S, E, I, IR, IH, R, newG)
-  
+  # run Markov 
   markov.df <- runMarkov(new.g.vec = newG,
                          trans.mat = trans.mat)
   
+  # add a day column 
   markov.df$day <- 0:(num.days-1)
   
+  # merge original and Markov dataframes
   df.return <- merge(df.return, markov.df, by = 'day')
   
-  # df.return$R <- df.return$R + df.return$disc
+  # processing some columns 
+  df.return$I <- df.return$IR + df.return$IH + df.return$G.state + 
+    df.return$ICU.state + df.return$V.state
+
+  df.return$R.orig <- df.return$R
+  df.return$R <- df.return$R + df.return$disc
   df.return$hosp <- df.return$G.state + df.return$ICU.state + df.return$V.state
   df.return$icu <- df.return$ICU.state + df.return$V.state
   df.return$vent <- df.return$V.state
   
   return(df.return)
-  
 }
 
+
+#' Process dataframe to download 
+#' 
+#' Processes download dataframe with columns with specified names in a
+#' specified order. 
+#'
+#' @param df Dataframe. 
+#'
+#' @return Dataframe. 
 process.df.for.download <- function(df){
+  
+  df$R <- df$R.orig
   df$HOSP.report <- df$hosp
   df$ICU.report <- df$icu 
   df$VENT.report <- df$vent
@@ -161,5 +216,4 @@ process.df.for.download <- function(df){
               'HOSP.report', 'ICU.report', 'VENT.report', 'DISCHARGE.report', 'MORTALITY.report')]
   
   return(df)
-
 }
